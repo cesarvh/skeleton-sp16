6124bf9d8b0a248754ae88283253143f1fbd8217
Cesar Villalobos <Cesarvh@Cesars-MacBook-Pro.local>
2016-02-04 23:25:01 -0800

    Adding starting files

---
+++ proj1/ArrayDeque.java
@@ -0,0 +1,30 @@
+public class ArrayDeque<Pokemon> {
+
+	public void addFirst(Pokemon item) {}
+
+	public void addLast(Pokemon item) {}
+
+	public boolean isEmpty() {
+		return true;
+	}
+	
+	public int size() {
+		return 0;
+	}
+
+	public void printDeque() {}
+
+	public Pokemon removeFirst() {
+		return null;
+	}
+
+	public Pokemon removeLast() {
+		return null;
+	}
+
+	public Pokemon get(int index) {
+		return null;
+	}
+
+	 // /Users/Cesarvh/Documents/CS/61B/sp16/skeleton-sp16/proj1/ArrayDeque.java
+}
